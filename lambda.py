d6548a860565877e19261b631549fc974d4f86ec
Ankush <ankush357159@gmail.com>
2021-12-11 23:56:32 +0530

    lamda

--- lambda.py
+++ lambda.py
@@ -1,3 +1,19 @@
-string = "Hello, how are you"
+# string = "Hello, how are you"
 
-print(lambda string: string)
+# print(lambda string: string)
+
+# double = lambda n: n*2
+
+# print(double(10))
+
+# larger = lambda a, b: a if a > b else b
+
+# print(larger(10,25))
+
+names = ["Ramanand", "Shyama", "Karan", "Jeevan", "Sandeep"]
+
+# names.sort()
+# names.sort(key = lambda x: len(x))
+names.sort(reverse=False)
+
+print(names)
