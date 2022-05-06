b6d7d2ee3fc2cde21a528cbbecaf178c6a02385f
Ankush <ankush357159@gmail.com>
2022-05-07 02:15:04 +0530

    Sum of Digits

--- sumOfDigits.py
+++ sumOfDigits.py
@@ -1,14 +1,17 @@
 # Python program to calculate sum of the digits of number
-def sumOfDigits(num):
-    temp = num
-    sum = 0
-    while temp > 0:
-        digit = temp % 10
-        sum += digit
-        temp //= 10
+def sumOfDigits(num1, num2):
+    for number in range(num1, num2 +1):
+
+        temp = number
+        sum = 0
+        while temp > 0:
+            digit = temp % 10
+            sum += digit
+            temp //= 10
 
         
-    print(sum)
+        print("Sum of the digits of {0} is {1} ".format(number, sum))
 
-num = int(input("Enter the number: "))
-sumOfDigits(num)
+num1 = int(input("Enter the number: "))
+num2 = int(input("Enter the number: "))
+sumOfDigits(num1, num2)
